5d47b3382f0d450222f8106c1ca56f735b85cace
Lakshya Tomar <63159708+lakshyaTomar@users.noreply.github.com>
2024-09-04 02:22:49 +0530

    Add files via upload

---
+++ MusicPlayer/src/MusicPlayer/Album.java
@@ -0,0 +1,79 @@
+package MusicPlayer;
+
+import java.util.*;
+
+public class Album {
+
+	private String name;
+	private String artist;
+	private ArrayList<Song> songs;
+
+	public Album(String name, String artist) {
+
+		this.name = name;
+		this.artist = artist;
+		this.songs = new ArrayList<Song>();
+
+	}
+
+	public Album() {
+
+	}
+
+	public Song findSong(String title) {
+
+		for (Song checkedSong : songs) {
+
+			if (checkedSong.getTitle().equals(title)) {
+
+				return checkedSong;
+
+			}
+		}
+
+		return null;
+
+	}
+
+	public boolean addSong(String title, double duration) {
+
+		if (findSong(title) == null) {
+			songs.add(new Song(title, duration));
+			System.out.println(title + "Added Successfully to the Album");
+			return true;
+		} else {
+			System.out.println(title + "Already exist in the album Album");
+			return false;
+		}
+	}
+
+	public boolean addToPlayList(int trackNumber, LinkedList<Song> PlayList) {
+
+		int index = trackNumber - 1;
+		if (index > 0 && index <= this.songs.size()) {
+
+			PlayList.add(this.songs.get(index));
+			return true;
+
+		}
+		System.out.println("This Album does not have song with track number " + trackNumber);
+		return false;
+
+	}
+
+	public boolean addToPlayList(String title, LinkedList<Song> PlayList) {
+
+		for (Song checkedSong : this.songs) {
+
+			if (checkedSong.getTitle().equals(title)) {
+				PlayList.add(checkedSong);
+				return true;
+			}
+
+		}
+		System.out.println(title + " song is not found in the album");
+		return false;
+
+	}
+
+}
